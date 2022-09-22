d8e9bb0b3d7f5a697933bda8843b6bf63363c3f4
jongchan <paekgga@postech.ac.kr>
2022-09-22 20:06:24 +0900

    sac+sesac branch

---
+++ run.sh
@@ -0,0 +1,5 @@
+python main.py --alg-name 'sac' --random-seed 800
+python main.py --alg-name 'sac' --random-seed 801
+python main.py --alg-name 'sac' --random-seed 802
+python main.py --alg-name 'sac' --random-seed 803
+python main.py --alg-name 'sac' --random-seed 804
